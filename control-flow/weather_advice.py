7dd92becf497648332744f34eb61eeed558049e9
Pharrel.O <pharrelthegr8@gmail.com>
2025-09-22 17:00:40 +0300

    Submit weather advice program

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -3,12 +3,11 @@ weather_conditions = ("sunny", "rainy", "cold")
 user_input = input("What's the weather like today? (sunny/rainy/cold): ")
 
 if user_input in weather_conditions:
-    match user_input:
-        case "sunny":
-            print("Wear a t-shirt and sunglasses.")
-        case "rainy":
-            print("Don't forget your umbrella and a raincoat.")
-        case "cold":
+    if user_input == "sunny":
+        print("Wear a t-shirt and sunglasses.")
+    elif user_input == "rainy":
+        print("Don't forget your umbrella and a raincoat.")
+    elif user_input == "cold":
             print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
