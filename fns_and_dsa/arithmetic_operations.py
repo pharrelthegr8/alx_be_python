de80c5dfb195b9c49198428d0acb6eb0015a1878
Pharrel.O <pharrelthegr8@gmail.com>
2025-09-30 00:14:27 +0300

    Submit arithmetic operations program

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -2,18 +2,17 @@ def perform_operation(num1, num2, operation):
     operations = ("add", "subtract", "multiply", "divide")
 
     if operation in operations:
-        match operation:
-            case "add":
-                return num1 + num2
-            case "subtract":
-                return num1 - num2
-            case "multiply":
-                return num1 * num2
-            case "divide":
-                if num2 != 0:
-                    return num1 / num2
-                else:
-                    print ("Error cannot divide by zero!")
+        if operation == "add":
+            return num1 + num2
+        elif operation == "subtract":
+            return num1 - num2
+        elif operation == "multiply":
+            return num1 * num2
+        elif operation == "divide":
+            if num2 != 0:
+                return num1 / num2
+            else:
+                print ("Error cannot divide by zero!")
     else:
         print ("The operation provided is Invalid!")
 
